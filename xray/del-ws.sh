318450e15902e2c4a3f1281f15573ea501efee21
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-15 21:13:04 +0700

    Update del-ws.sh

--- xray/del-ws.sh
+++ xray/del-ws.sh
@@ -103,6 +103,6 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
     echo " Expired On  : $exp"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo ""
-    read -n 1 -s -r -p "Press any key to back on menu
+    read -n 1 -s -r -p "Press any key to back on menu"
     menu
     fi
