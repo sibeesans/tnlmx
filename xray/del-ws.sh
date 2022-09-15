f400bf5ab58c059f2c0576e45003fad47b1d2b9b
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-15 20:11:12 +0700

    Update del-ws.sh

--- xray/del-ws.sh
+++ xray/del-ws.sh
@@ -90,7 +90,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 	read -rp "Input Username : " user
     if [ -z $user ]; then
-    v2ray-menu
+    menu
     else
     exp=$(grep -wE "^### $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
     sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
@@ -103,7 +103,6 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
     echo " Expired On  : $exp"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo ""
-    read -n 1 -s -r -p "Press any key to back on menu"
-    
+    read -n 1 -s -r -p "Press any key to back on menu
     menu
     fi
